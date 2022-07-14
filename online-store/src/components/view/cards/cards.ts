2e677e59b939ef384f175ee87368ed61d623bd4d
Marcus <marikord26@gmail.com>
2022-07-14 18:44:09 +0200

    feat: draw cards from database

---
+++ online-store/src/components/view/cards/cards.ts
@@ -0,0 +1,55 @@
+import { IData } from "../../types/types";
+import './cards.css'
+
+class Cards {
+  draw(data: IData[]): void {
+    const catalog = document.querySelector('.catalog') as HTMLElement
+    const dataSorted: IData[] = data.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0
+    })
+    dataSorted.forEach(item => {
+      const catalogItem = document.createElement('div') as HTMLElement
+      const catalogItemTitle = document.createElement('h3') as HTMLElement
+      const catalogItemImg = document.createElement('div') as HTMLElement
+      const catalogItemQuantity = document.createElement('p') as HTMLElement
+      const catalogItemYear = document.createElement('p') as HTMLElement
+      const catalogItemComp = document.createElement('p') as HTMLElement
+      const catalogItemColor = document.createElement('p') as HTMLElement
+      const catalogItemSize = document.createElement('p') as HTMLElement
+
+      catalog.append(catalogItem)
+      catalogItem.classList.add('catalog__item')
+
+
+      catalogItem.append(catalogItemTitle)
+      catalogItemTitle.textContent = item.name
+
+      catalogItem.append(catalogItemImg)
+      catalogItemImg.classList.add('item__img')
+      catalogItemImg.style.backgroundImage = `url("${item.img}")`
+
+      catalogItem.append(catalogItemQuantity)
+      catalogItemQuantity.textContent = `In stock: ${item.quantity}`
+
+      catalogItem.append(catalogItemYear)
+      catalogItemYear.textContent = `Produced in: ${item.year}`
+
+      catalogItem.append(catalogItemComp)
+      catalogItemComp.textContent = `Producent: ${item.shape}`
+
+      catalogItem.append(catalogItemColor)
+      catalogItemColor.textContent = `Color: ${item.color}`
+
+      catalogItem.append(catalogItemSize)
+      catalogItemSize.textContent = `Cameras qty.: ${item.size}`
+    })
+  }
+}
+
+export default Cards
---
+++ online-store/src/index.ts
@@ -0,0 +1,6 @@
+import { data } from "./components/data/data";
+import Cards from "./components/view/cards/cards";
+import './global.css'
+
+const test = new Cards()
+test.draw(data)
